aba3da0b02ea9103db308202e4641779d40e5dbf
Nurguly Ashyrov <nurguly.ashyrov@gmail.com>
2015-07-28 20:53:02 +0500

    Use `$config->babelHomePage` instead of `get_home_page`

--- markups/default.php
+++ markups/default.php
@@ -4,5 +4,5 @@ include("../WidgetSlideMenuRenderer.php");
 
 $renderer = new WidgetSlideMenuRenderer(wire('page'), $settings);
 
-echo "<div class='m-l--w' id='m_l__w'>". $renderer->render_menu(get_home_page(), $settings->menu_level) ."</div><!-- m-l--w -->";
+echo "<div class='m-l--w' id='m_l__w'>". $renderer->render_menu(wire('config')->babelHomePage, $settings->menu_level) ."</div><!-- m-l--w -->";
 
